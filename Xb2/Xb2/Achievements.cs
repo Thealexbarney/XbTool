1befc76a3620c33fcebc995bfa91bbdd1f569609
Alex Barney <thealexbarney@gmail.com>
2018-03-13 20:54:54 -0500

    Make GitHub's CSV parser happy

--- Xb2/Xb2/Achievements.cs
+++ Xb2/Xb2/Achievements.cs
@@ -1,6 +1,8 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using CsvHelper;
+using CsvHelper.Configuration;
 using Xb2.Bdat;
 using Xb2.BdatString;
 using Xb2.Types;
@@ -11,64 +13,64 @@ namespace Xb2
     {
         public static void PrintAchievements(BdatCollection tables, StreamWriter writer)
         {
-            var csv = new CsvWriter(writer);
-            writer.WriteLine("Blade ID,Blade Name,Skill,Type,Col,Level,Idea Category,Idea Points,Condition,Count,Result");
-            PrintBladeAchievements(tables.CHR_Bl, csv);
+            var achievements = GetBladeAchievements(tables.CHR_Bl);
+            var csv = new CsvWriter(writer, new Configuration { HasHeaderRecord = false });
+            writer.WriteLine("Blade ID,Blade Name,Skill,Type,Col,Row,Level,Idea Category,Idea Points,Condition,Count,Result");
+            csv.WriteRecords(achievements);
         }
 
-        private static void PrintAchievement(CsvWriter csv, CHR_Bl blade, FLD_AchievementSet set, string skillName, string type, int column)
+        private static IEnumerable<Achievement> GetAchievementSet(CHR_Bl blade, FLD_AchievementSet set, string skillName, string type, int column)
         {
+            var achievements = new List<Achievement>();
+            int level = 1;
             for (int i = 0; i < set._AchievementID.Length; i++)
             {
                 var achieve = set._AchievementID[i];
-                FLD_QuestList quest = achieve?._Task;
+                var output = new Achievement();
+                achievements.Add(output);
 
-                csv.WriteField(blade.Id);
-                csv.WriteField(blade._Name.name);
-                csv.WriteField(skillName);
-                csv.WriteField(type);
-                csv.WriteField(column);
-                csv.WriteField(i + 1);
-                if (quest == null)
-                {
-                    csv.NextRecord();
-                    continue;
-                }
+                output.BladeId = blade.Id;
+                output.BladeName = blade._Name.name;
+                output.Skill = skillName;
+                output.Type = type;
+                output.Col = column;
+                output.Row = i + 1;
+                if (achieve == null) continue;
+
+                output.Level = level++.ToString();
+                FLD_QuestList quest = achieve._Task;
+                if (quest == null) continue;
 
                 FLD_QuestTask task = quest._NextQuestA._PurposeID;
                 int ideaPoints = quest._RewardSetA.IdeaValue;
                 var ideaCategory = (IdeaCategory)quest._RewardSetA.IdeaCategory;
 
-                csv.WriteField(ideaPoints > 0 ? ideaCategory.ToString() : "");
-                csv.WriteField(ideaPoints > 0 ? ideaPoints.ToString() : "");
-                csv.WriteField(task._TaskLog1.name);
-                csv.WriteField(GetTaskCount(task) > 0 ? GetTaskCount(task).ToString() : "");
-                csv.WriteField(achieve._Task._ResultA.name);
-                csv.NextRecord();
+                output.IdeaCategory = ideaPoints > 0 ? ideaCategory.ToString() : "";
+                output.IdeaPoints = ideaPoints > 0 ? ideaPoints.ToString() : "";
+                output.Condition = task._TaskLog1.name;
+                output.Count = GetTaskCount(task) > 0 ? GetTaskCount(task).ToString() : "";
+                output.Result = achieve._Task._ResultA.name;
             }
+
+            return achievements;
         }
 
-        private static void PrintBladeAchievements(BdatTable<CHR_Bl> tables, CsvWriter csv)
+        private static IEnumerable<Achievement> GetBladeAchievements(BdatTable<CHR_Bl> tables)
         {
+            var achievements = new List<Achievement>();
             foreach (CHR_Bl blade in tables.Items.Where(x => x._ArtsAchievement1 != null))
             {
-                PrintAchievement(csv, blade, blade._KeyAchievement, string.Empty, "Key", 1);
+                achievements.AddRange(GetAchievementSet(blade, blade._KeyAchievement, string.Empty, "Key", 1));
 
                 for (int i = 0; i < 3; i++)
                 {
-                    PrintAchievement(csv, blade, blade._ArtsAchievement[i], blade._BArts[i]?._Name.name, "Special", 2 + i);
-                }
-
-                for (int i = 0; i < 3; i++)
-                {
-                    PrintAchievement(csv, blade, blade._SkillAchievement[i], blade._BSkill[i]?._Name.name, "Battle Skill", 5 + i);
-                }
-
-                for (int i = 0; i < 3; i++)
-                {
-                    PrintAchievement(csv, blade, blade._FskillAchivement[i], blade._FSkill[i]?._Name.name, "Field Skill", 8 + i);
+                    achievements.AddRange(GetAchievementSet(blade, blade._ArtsAchievement[i], blade._BArts[i]?._Name.name, "Special", 2 + i));
+                    achievements.AddRange(GetAchievementSet(blade, blade._SkillAchievement[i], blade._BSkill[i]?._Name.name, "Battle Skill", 5 + i));
+                    achievements.AddRange(GetAchievementSet(blade, blade._FskillAchivement[i], blade._FSkill[i]?._Name.name, "Field Skill", 8 + i));
                 }
             }
+
+            return achievements.OrderBy(x => x.BladeId).ThenBy(x => x.Col).ThenBy(x => x.Row);
         }
 
         private static int GetTaskCount(FLD_QuestTask questTask)
@@ -91,6 +93,22 @@ namespace Xb2
                 default:
                     return -1;
             }
+        }
+
+        private class Achievement
+        {
+            public int BladeId { get; set; }
+            public string BladeName { get; set; }
+            public string Skill { get; set; }
+            public string Type { get; set; }
+            public int Col { get; set; }
+            public int Row { get; set; }
+            public string Level { get; set; }
+            public string IdeaCategory { get; set; }
+            public string IdeaPoints { get; set; }
+            public string Condition { get; set; }
+            public string Count { get; set; }
+            public string Result { get; set; }
         }
     }
 }
