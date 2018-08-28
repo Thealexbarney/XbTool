fe5110701ab171ee82abf0fada0ad3c4b148c03e
Alex Barney <thealexbarney@gmail.com>
2018-08-28 14:36:45 -0500

    Fix agility on enemy printout

--- XbTool/XbTool/Xb2/Enemies.cs
+++ XbTool/XbTool/Xb2/Enemies.cs
@@ -35,7 +35,7 @@ namespace XbTool.Xb2
                 en.Strength = (int)(enemy._ParamID.StrengthRev * 0.001 * lvParam.StrengthBase * ((enemy._ParamRev?.StrengthRev ?? 1000) * 0.001));
                 en.Ether = (int)(enemy._ParamID.PowEtherRev * 0.001 * lvParam.PowEtherBase * ((enemy._ParamRev?.PowEtherRev ?? 1000) * 0.001));
                 en.Dexterity = (int)(enemy._ParamID.DexRev * 0.001 * lvParam.DexBase * ((enemy._ParamRev?.DexRev ?? 1000) * 0.001));
-                en.Agility = (int)(enemy._ParamID.AiID * 0.001 * lvParam.AgilityBase * ((enemy._ParamRev?.AgilityRev ?? 1000) * 0.001));
+                en.Agility = (int)(enemy._ParamID.AgilityRev * 0.001 * lvParam.AgilityBase * ((enemy._ParamRev?.AgilityRev ?? 1000) * 0.001));
                 en.Strength = (int)(enemy._ParamID.StrengthRev * 0.001 * lvParam.StrengthBase * ((enemy._ParamRev?.StrengthRev ?? 1000) * 0.001));
                 en.Luck = (int)(enemy._ParamID.LuckRev * 0.001 * lvParam.LuckBase * ((enemy._ParamRev?.LuckRev ?? 1000) * 0.001));
                 en.MaxHp = (int)(enemy._ParamID.HpMaxRev * 0.001 * lvParam.HpMaxBase * ((enemy._ParamRev?.HpMaxRev ?? 1000) * 0.001));
@@ -74,7 +74,6 @@ namespace XbTool.Xb2
             public int Gold { get; set; }
             public int Wp { get; set; }
             public int Sp { get; set; }
-            public string Enhance1 { get; set; }
         }
     }
 }
